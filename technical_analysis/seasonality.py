ba3a514d572f7173a2655dce2c86971fd38f0e5b
dev <dev@psyberdyne.net>
2017-03-06 13:30:02 +0700

    Sesonality refactoring

--- technical_analysis/seasonality.py
+++ technical_analysis/seasonality.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
 import datetime
 from dateutil.relativedelta import relativedelta
 import logging as logger
@@ -5,6 +8,31 @@ import logging as logger
 import pandas as pd
 
 logger.basicConfig(level=logger.INFO, format='%(filename)s: %(message)s')
+
+
+class Seasonality:
+    """
+    To be used by backtester, scanning code etc.
+    For example initialize once when starting scanner tool:
+        seasonality = Seasonality('SPY', spy_daily)
+
+    Then call with for every latest bar available:
+        trading_day = spy_daily.loc['20151103']['Day']
+        if long_conditions(spy_daily) and seasonality.average_return(trading_day, 22) > 0.0:
+            enter_long()
+    """
+    def __init__(self, ticker, daily):
+        self.data = trading_day_reindex(seasonality_returns(daily), ticker, ticker)
+
+    def average_return(self, trading_day, forward_days):
+        """
+        Get the seasonality return from trading_day to forward_days.
+        Should probably use multiple calls with different forward dates to get more reliable prediction.
+        (dates differ year to year and seasonality df is reindexed to shortest year in analysis)
+        """
+        rotational_index = int(trading_day + forward_days) % len(self.data.index)
+        rotations = int((trading_day + forward_days) / len(self.data.index))
+        return ((self.data.iloc[rotational_index] + rotations * 100) - self.data.iloc[int(trading_day)])[0]
 
 
 def rebase(prices):
@@ -47,6 +75,7 @@ def __normalized_per_year(df):
     dates = min_group[1].index
 
     return normalized.dropna().set_index(dates)
+
 
 def __normalized_per_month(df):
     monthly_averages = []
@@ -100,6 +129,7 @@ def seasonality_monthly_returns(df):
 
     return normalized_monthly
 
+
 def seasonality_returns(df):
     """
     Calculate seasonality for ticker. Years to include is based the first and last index, e.g. 2005-01-01 to 2015-12-31
@@ -133,13 +163,3 @@ def trading_day_reindex(df, ticker, column):
     days = range(1, len(df) - 1)
     return pd.DataFrame({ticker: df.ix[:, column].values}).reindex(days)
 
-
-def average_return(seasonality_df, trading_day, forward_days):
-    """
-    Get the seasonality return from trading_day to forward_days.
-    Should probably use multiple calls with different forward dates to get more reliable prediction.
-    (dates differ year to year and seasonality df is reindexed to shortest year in analysis)
-    """
-    rotational_index = int(trading_day + forward_days) % len(seasonality_df.index)
-    rotations = int((trading_day + forward_days) / len(seasonality_df.index))
-    return ((seasonality_df.iloc[rotational_index] + rotations * 100) - seasonality_df.iloc[int(trading_day)])[0]
--- test/test_seasonality.py
+++ test/test_seasonality.py
@@ -1,7 +1,7 @@
 import unittest
 
 from dataprovider.dataprovider import CachedDataProvider
-from technical_analysis import seasonality
+from technical_analysis.seasonality import Seasonality
 
 
 class TestSeasonality(unittest.TestCase):
@@ -9,22 +9,21 @@ class TestSeasonality(unittest.TestCase):
 
     def test_average_return(self):
         spy_daily = self.local_provider.get_data(ticker="spy", from_date='2010-01-01', to_date='2016-01-01')
-
-        df = seasonality.trading_day_reindex(seasonality.seasonality_returns(spy_daily), "SPY", "SPY")
+        s = Seasonality("SPY", spy_daily)
 
         # Bullish Nov to May
-        assert seasonality.average_return(df, spy_daily.loc['20151103']['Day'], 22) > 0.0  # Nov-Dec
-        assert seasonality.average_return(df, spy_daily.loc['20151103']['Day'], 22 * 2) > 0.0  # Nov-Jan
-        assert seasonality.average_return(df, spy_daily.loc['20151103']['Day'], 22 * 3) > 0.0  # Nov-Feb
-        assert seasonality.average_return(df, spy_daily.loc['20151103']['Day'], 22 * 4) > 0.0  # Nov-Mar
-        assert seasonality.average_return(df, spy_daily.loc['20151103']['Day'], 22 * 5) > 0.0  # Nov-May
+        assert s.average_return(spy_daily.loc['20151103']['Day'], 22) > 0.0  # Nov-Dec
+        assert s.average_return(spy_daily.loc['20151103']['Day'], 22 * 2) > 0.0  # Nov-Jan
+        assert s.average_return(spy_daily.loc['20151103']['Day'], 22 * 3) > 0.0  # Nov-Feb
+        assert s.average_return(spy_daily.loc['20151103']['Day'], 22 * 4) > 0.0  # Nov-Mar
+        assert s.average_return(spy_daily.loc['20151103']['Day'], 22 * 5) > 0.0  # Nov-May
 
         # Bearish May to Nov
-        assert seasonality.average_return(df, spy_daily.loc['20150501']['Day'], 22) < 0.0  # May-June
-        assert seasonality.average_return(df, spy_daily.loc['20150501']['Day'], 22 * 2) < 0.0  # May-Jul
-        assert seasonality.average_return(df, spy_daily.loc['20150501']['Day'], 22 * 5) < 0.0  # May-Oct
+        assert s.average_return(spy_daily.loc['20150501']['Day'], 22) < 0.0  # May-June
+        assert s.average_return(spy_daily.loc['20150501']['Day'], 22 * 2) < 0.0  # May-Jul
+        assert s.average_return(spy_daily.loc['20150501']['Day'], 22 * 5) < 0.0  # May-Oct
 
-        # assert seasonality.py.average_return('spy', '2016-11-05', '5M') > 0.0  # Throw error, lookahead bias?
+        # assert seasonality_analysis.py.average_return('spy', '2016-11-05', '5M') > 0.0  # Throw error, lookahead bias?
 
 
 if __name__ == '__main__':

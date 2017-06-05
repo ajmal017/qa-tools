acdcd5c330069b0c085c6d754a5f232ef3c71caa
fbjarkes <fredrik.bjarkeson@gmail.com>
2017-06-05 19:06:35 +0200

    fixed tests path etc.

--- tests/test_market_internal.py
+++ tests/test_market_internal.py
@@ -3,7 +3,10 @@
 
 import unittest
 
-from qa_dataprovider.web_dataprovider import CachedDataProvider
+from qa_dataprovider.web_dataprovider import CachedWebDataProvider
+
+import sys
+sys.path.append("../")
 
 from technical_analysis import ta
 import technical_analysis.column_names as ta_columns
@@ -11,21 +14,21 @@ from technical_analysis.market_internal import MarketInternals
 
 
 class TestMarketInternals(unittest.TestCase):
-    testdata_provider = CachedDataProvider(cache_name='tests', expire_days=0, quote=False)
+    testdata_provider = CachedWebDataProvider('google', cache_name='tests', expire_days=0, quote=False)
 
     def test_market_breadth(self):
-        aapl = self.testdata_provider.get_data(ticker="AAPL", from_date='2016-01-01', to_date='2017-01-01')
-        msft = self.testdata_provider.get_data(ticker="MSFT", from_date='2016-01-01', to_date='2017-01-01')
-        gm = self.testdata_provider.get_data(ticker="GM", from_date='2016-01-01', to_date='2017-01-01')
+        aapl = self.testdata_provider.get_data(['AAPL'], from_date='2016-01-01', to_date='2017-01-01')[0]
+        msft = self.testdata_provider.get_data(['MSFT'], from_date='2016-01-01', to_date='2017-01-01')[0]
+        gm = self.testdata_provider.get_data(['GM'], from_date='2016-01-01', to_date='2017-01-01')[0]
         tickers = [aapl, msft, gm]
 
         breadth = MarketInternals().breadth(tickers, 50, '2016-01-01', '2017-01-01', MarketInternals.hilo)
         assert 0 == breadth.loc['20161223'][ta_columns.day_low_name(50)]
 
     def test_market_breadth_dma(self):
-        aapl = self.testdata_provider.get_data(ticker="AAPL", from_date='2010-01-01', to_date='2017-01-01')
-        msft = self.testdata_provider.get_data(ticker="MSFT", from_date='2010-01-01', to_date='2017-01-01')
-        gm = self.testdata_provider.get_data(ticker="GM", from_date='2010-01-01', to_date='2017-01-01')
+        aapl = self.testdata_provider.get_data(['AAPL'], from_date='2010-01-01', to_date='2017-01-01')[0]
+        msft = self.testdata_provider.get_data(['MSFT'], from_date='2010-01-01', to_date='2017-01-01')[0]
+        gm = self.testdata_provider.get_data(['GM'], from_date='2010-01-01', to_date='2017-01-01')[0]
         tickers = [aapl, msft, gm]
 
         for ticker in tickers:
--- tests/test_weekly_tight_range.py
+++ tests/test_weekly_tight_range.py
@@ -3,18 +3,22 @@
 
 import unittest
 
-from qa_dataprovider.web_dataprovider import CachedDataProvider
+import sys
+sys.path.append("../")
+
+from qa_dataprovider.web_dataprovider import CachedWebDataProvider
 from technical_analysis.weekly_tight_range import WeeklyTightRange
 
 
+
 class TestWeeklyTightRange(unittest.TestCase):
-    local_provider = CachedDataProvider(cache_name='tests', expire_days=0, trading_days=True)
+    local_provider = CachedWebDataProvider('google', cache_name='tests', expire_days=0, trading_days=True)
 
     def test_previous_week_is_tight_range(self):
-        aapl_daily = self.local_provider.get_data(ticker="aapl", from_date='2011-08-01', to_date='2016-01-01',
-                                                  timeframe='day')
-        aapl_weekly = self.local_provider.get_data(ticker="aapl", from_date='2011-08-01', to_date='2016-01-01',
-                                                   timeframe='week')
+        aapl_daily = self.local_provider.get_data(['AAPL'], from_date='2011-08-01', to_date='2016-01-01',
+                                                  timeframe='day')[0]
+        aapl_weekly = self.local_provider.get_data(['AAPL'], from_date='2011-08-01', to_date='2016-01-01',
+                                                   timeframe='week')[0]
 
         wtr = WeeklyTightRange('appl', 3, aapl_weekly)
         assert wtr.get_prev_tight_range_week(aapl_daily.loc['20120123'].name.date())  # Breaking previous weeks high?
